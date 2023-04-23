81e266dd0ab97bad5d61209470c237c22ad5790b
Kamil <k.wypych@pap.pl>
2023-04-23 14:27:26 +0200

    refactor: first config, config eslint, prettier

---
+++ ticketly/.eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: true,
+        semi: true,
+        printWidth: 120,
+      },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-no-comment-textnodes': 0,
+    'react/button-has-type': 0,
+    '@typescript-eslint/no-shadow': 0,
+    'import/prefer-default-export': 0,
+    'react/function-component-definition': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'react/require-default-props': 0,
+    'eslint import/no-absolute-path': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['src/', 'node_modules'],
+      },
+    },
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
+  },
+};
