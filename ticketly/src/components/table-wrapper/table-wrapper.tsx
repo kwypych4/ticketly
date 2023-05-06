ea124ca9b57fe33ef8e492e999babe088b244c6b
Kamil <k.wypych@pap.pl>
2023-05-06 10:50:28 +0200

    refactor: created table wrapper

---
+++ ticketly/src/components/table-wrapper/table-wrapper.tsx
@@ -0,0 +1,40 @@
+import { TableProps } from 'antd';
+import { cloneElement, Dispatch, SetStateAction } from 'react';
+import { PageItem } from 'styles';
+import { RequestParamsType } from 'types';
+import { WithPaginationTableType } from 'types/table';
+
+type TableWrapperProps = {
+  children: JSX.Element;
+  setOptions: Dispatch<SetStateAction<RequestParamsType | undefined>>;
+  data: WithPaginationTableType<any>;
+};
+
+export const TableWrapper = ({ children, setOptions, data }: TableWrapperProps) => {
+  const handleTableChange: TableProps<string>['onChange'] = (pagination, filters, sorter) => {
+    const params: RequestParamsType = {
+      ...(pagination.current && { page: pagination.current, limit: pagination.pageSize }),
+      ...('field' in sorter && 'order' in sorter && sorter.order && { sort: `${sorter.field}:${sorter.order}` }),
+    };
+    if (filters)
+      Object.entries(filters)
+        .filter(([_, val]) => val !== null)
+        .forEach(([key, val]) => {
+          params[key] = val;
+        });
+    setOptions(params);
+  };
+
+  const childrenWithProps = cloneElement(children, {
+    setOptions: handleTableChange,
+    data: data?.data,
+    pagination: data?.pagination,
+  });
+
+  return (
+    <PageItem>
+      <h3>hello</h3>
+      {childrenWithProps}
+    </PageItem>
+  );
+};
