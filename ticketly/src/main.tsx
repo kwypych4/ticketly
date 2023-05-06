b085d988a197bf873b26ffb018e1c5dec521d1b9
Kamil <k.wypych@pap.pl>
2023-05-06 10:47:14 +0200

    refactor: added Query provider and antD global styles

--- ticketly/src/main.tsx
+++ ticketly/src/main.tsx
@@ -1,12 +1,26 @@
+import { App } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from 'router';
 import { GlobalStyle } from 'styles';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 20000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <RouterProvider router={Router} />
+    <QueryClientProvider client={queryClient}>
+      <App>
+        <GlobalStyle />
+        <RouterProvider router={Router} />
+      </App>
+    </QueryClientProvider>
   </React.StrictMode>
 );
