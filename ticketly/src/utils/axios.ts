b302b230ead04e266759b78b42a2ad9d3470e749
Kamil <k.wypych@pap.pl>
2023-05-06 10:47:59 +0200

    refactor: added axios fetching instance

---
+++ ticketly/src/utils/axios.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios';
+import { RequestParamsType } from 'types';
+
+const baseURL = import.meta.env.DEV ? import.meta.env.VITE_APP_DEV_API_BASE : import.meta.env.VITE_APP_PROD_API_BASE;
+
+export const axiosInstance = axios.create({
+  baseURL,
+});
+
+export const request = <T, Y>(
+  url: string,
+  method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
+  params?: RequestParamsType
+): Promise<AxiosResponse<T, Y>> =>
+  axiosInstance({
+    method,
+    url,
+    params,
+  });
