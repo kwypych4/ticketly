ea752014d7d5478b6b55ac3d3c3e15cc8790acb8
Kamil <k.wypych@pap.pl>
2023-05-09 23:16:37 +0200

    refactor: created interceptors and improved axios instance

--- ticketly/src/utils/axios.ts
+++ ticketly/src/utils/axios.ts
@@ -1,23 +1,57 @@
-import axios, { AxiosResponse } from 'axios';
+import { refreshAccessToken } from 'api/auth';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useAuthStore } from 'store';
 import { RequestParamsType } from 'types';
 
 const baseURL = import.meta.env.DEV ? import.meta.env.VITE_APP_DEV_API_BASE : import.meta.env.VITE_APP_PROD_API_BASE;
 
 export const axiosInstance = axios.create({
   baseURL,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': 'true',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  },
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError<{ error: string }>) => {
+    const { accessToken } = useAuthStore.getState();
+
+    if (
+      error?.response?.status === 401 &&
+      (!accessToken || error.response?.data.error === 'Unauthorized - the access token has expired!')
+    ) {
+      try {
+        const accessTokenQuery = await refreshAccessToken();
+        useAuthStore.setState({ accessToken: accessTokenQuery.accessToken });
+      } catch (error) {
+        alert('Nie można pobrać refresh tokena');
+        useAuthStore.setState({ isLogged: false });
+      }
+    }
+  }
+);
 
 export const request = <T, Y>(
   url: string,
   method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
   params?: RequestParamsType,
   body?: RequestParamsType
-): Promise<AxiosResponse<T, Y>> =>
-  axiosInstance({
+): Promise<AxiosResponse<T, Y>> => {
+  const { accessToken } = useAuthStore.getState();
+
+  return axiosInstance({
     method,
     url,
     params,
     data: {
       ...body,
     },
+    headers: {
+      ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+    },
   });
+};
