bfdfc926f173c58dfa023ab1950ae92655ece84e
Kamil <k.wypych@pap.pl>
2023-05-09 23:09:27 +0200

    refactor: added route for auth layout

--- ticketly/src/router.tsx
+++ ticketly/src/router.tsx
@@ -1,14 +1,19 @@
-import { LoggedLayout } from 'layouts';
-import { Homepage, TicketsDetailsPage, TicketsPage } from 'pages';
+import { AuthLayout, LoggedLayout } from 'layouts';
+import { Homepage, LoginPage, TicketsDetailsPage, TicketsPage } from 'pages';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { appRoutes } from 'urls';
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<LoggedLayout />}>
-      <Route path={appRoutes.app.homepage} element={<Homepage />} />
-      <Route path={appRoutes.app.tickets.index} element={<TicketsPage />} />
-      <Route path={appRoutes.app.tickets.details} element={<TicketsDetailsPage />} />
-    </Route>
+    <>
+      <Route element={<LoggedLayout />}>
+        <Route path={appRoutes.app.homepage} element={<Homepage />} />
+        <Route path={appRoutes.app.tickets.index} element={<TicketsPage />} />
+        <Route path={appRoutes.app.tickets.details} element={<TicketsDetailsPage />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path={appRoutes.auth.login} element={<LoginPage />} />
+      </Route>
+    </>
   )
 );
