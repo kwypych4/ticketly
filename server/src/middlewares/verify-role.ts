a16254220a64db944afa958938b09b8b7ffb1dd7
Kamil <k.wypych@pap.pl>
2023-05-16 20:29:10 +0200

    refactor: improved verify role middleware

--- server/src/middlewares/verify-role.ts
+++ server/src/middlewares/verify-role.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { UserRoles } from 'types';
 
 export const verifyRole = (privilegedRole: UserRoles[]) => (req: Request, res: Response, next: NextFunction) => {
-  if (privilegedRole.includes(req.role)) throw new HttpError(401, 'Unauthorized - no permissions');
+  if (!privilegedRole.includes(req.role)) throw new HttpError(401, 'Unauthorized - no permissions');
 
   next();
 };
