9f429c7b9012a88ca8891d7bc0c2d47f145b0916
Kamil <k.wypych@pap.pl>
2023-05-18 01:12:50 +0200

    refactor: improved protected route

--- ticketly/src/components/protected-route/protected-route.tsx
+++ ticketly/src/components/protected-route/protected-route.tsx
@@ -1,18 +1,20 @@
 import { api } from 'api';
-import { useQuery } from 'react-query';
+import { useCustomQuery } from 'hooks/use-custom-query';
 import { Navigate } from 'react-router-dom';
-import { useAuthStore } from 'store';
+import { useAuthStore, useUserStore } from 'store';
 import { appRoutes } from 'urls';
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const isLogged = useAuthStore((state) => state.isLogged);
 
-  const isLoggedQuery = useQuery(['checkLogin'], () => api.auth.checkLogin(), {
-    onSuccess: ({ accessToken }) => {
+  const isLoggedQuery = useCustomQuery(['checkLogin'], () => api.auth.checkLogin(), {
+    onSuccess: ({ accessToken, uid, username }) => {
       useAuthStore.setState({ isLogged: true, accessToken });
+      useUserStore.setState({ userId: uid, username });
     },
     onError: () => {
       useAuthStore.setState({ isLogged: false, accessToken: '' });
+      useUserStore.setState({ userId: '', username: '' });
     },
     staleTime: Infinity,
     retry: false,
