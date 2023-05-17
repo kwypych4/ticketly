32305bea98feb3ddf02e2d34ffa044e3268fa97d
Kamil <k.wypych@pap.pl>
2023-05-18 01:14:18 +0200

    refactor: added modal with form

--- ticketly/src/components/ticket-details/components/comments/comments.tsx
+++ ticketly/src/components/ticket-details/components/comments/comments.tsx
@@ -1,7 +1,10 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Form, Modal } from 'antd';
+import { useState } from 'react';
 import { CommentsListTypes } from 'types';
 
+import { AddCommentForm } from '../add-comment-form/add-comment-form';
 import { CommentsContent, CommentsTitle, CommentsWrapper } from '.';
 import { Comment } from './components/comment';
 
@@ -10,11 +13,13 @@ type CommentsProps = {
 };
 
 export const Comments = ({ comments }: CommentsProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const [form] = Form.useForm();
   return (
     <CommentsWrapper>
       <CommentsTitle>
         <h2>Comments</h2>
-        <button>
+        <button onClick={() => setShowModal(true)}>
           <FontAwesomeIcon icon={faPlus} />
           Add comment
         </button>
@@ -24,6 +29,9 @@ export const Comments = ({ comments }: CommentsProps) => {
           <Comment key={comment.id} commentData={comment} />
         ))}
       </CommentsContent>
+      <Modal title='Add comment' open={showModal} onOk={() => form.submit()} onCancel={() => setShowModal(false)}>
+        <AddCommentForm form={form} setShowModal={setShowModal} />
+      </Modal>
     </CommentsWrapper>
   );
 };
