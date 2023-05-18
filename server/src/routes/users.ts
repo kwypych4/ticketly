693ed0a57c5e339612fb443c31a50d20375d53e2
Kamil <k.wypych@pap.pl>
2023-05-19 01:37:52 +0200

    refactor: added paths for filters and user creating

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get('/', users.getUsers);
 
+router.get('/filters', users.getUsersFilters);
+
 router.get('/:id', users.getOneUser);
+
+router.post('/', users.createUser);
 
 router.delete('/:id', users.deleteUser);
 
