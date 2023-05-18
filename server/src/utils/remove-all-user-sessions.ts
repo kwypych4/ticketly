a242faca39d50b6c76509778f18732491d4d0502
Kamil <k.wypych@pap.pl>
2023-05-19 01:36:14 +0200

    refactor: improved deleting sesions taking user id

--- server/src/utils/remove-all-user-sessions.ts
+++ server/src/utils/remove-all-user-sessions.ts
@@ -1,12 +1,22 @@
 import { Request } from 'express';
 import { JwtType } from 'types';
 
-export const removeAllUserSessions = (req: Request, refreshToken: JwtType | null) =>
+type PropsWithRefreshTokenType = { req: Request; refreshToken: JwtType | null; userId?: never };
+type PropsWithUserIdType = { req: Request; refreshToken?: never; userId: string };
+
+type FnProps = PropsWithRefreshTokenType | PropsWithUserIdType;
+
+export const removeAllUserSessions = ({ req, refreshToken, userId }: FnProps) =>
   req.sessionStore.all?.((_, sessions) =>
     [sessions]?.forEach((session) => {
       if (refreshToken && session) {
         Object.entries(session).forEach(([key, value]) => {
           if (value.userId === refreshToken.userId) req.sessionStore.destroy(key);
+        });
+      }
+      if (userId && session) {
+        Object.entries(session).forEach(([key, value]) => {
+          if (String(value.userId) === userId) req.sessionStore.destroy(key);
         });
       }
     })
