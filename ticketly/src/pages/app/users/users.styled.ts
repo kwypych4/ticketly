1cb3a668ebabb2e0429bfef9944b29a84f28344d
Kamil <k.wypych@pap.pl>
2023-05-19 09:46:10 +0200

    feat: created styled container for buttons

---
+++ ticketly/src/pages/app/users/users.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  height: 100%;
+  align-items: center;
+  margin-left: auto;
+  gap: 12px;
+`;
