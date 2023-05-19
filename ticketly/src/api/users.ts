51faac2558d8d7c4810387a1a554d2b0006e93dc
Kamil <k.wypych@pap.pl>
2023-05-19 09:44:10 +0200

    feat: created api fn for creating & deleting user

--- ticketly/src/api/users.ts
+++ ticketly/src/api/users.ts
@@ -1,4 +1,12 @@
-import { RequestParamsType, UsersListTypes, WithPaginationTableType } from 'types';
+import {
+  CreateUserRequestTypes,
+  CreateUserResponseTypes,
+  DeleteUserResponseType,
+  RequestParamsType,
+  UsersFiltersTypes,
+  UsersListTypes,
+  WithPaginationTableType,
+} from 'types';
 import { apiUrls } from 'urls';
 import { request } from 'utils/axios';
 
@@ -14,4 +22,63 @@ const fetchList = async (options?: RequestParamsType): UsersListReturn => {
 
 export const list = {
   fetch: fetchList,
+};
+
+type UsersFiltersResponse = UsersFiltersTypes;
+type UsersFiltersRequest = object;
+type UsersFiltersReturn = Promise<UsersFiltersTypes>;
+
+const fetchFilters = async (): UsersFiltersReturn => {
+  const { data } = await request<UsersFiltersResponse, UsersFiltersRequest>(apiUrls.users.filters(), 'GET');
+
+  return data;
+};
+
+export const filters = {
+  fetch: fetchFilters,
+};
+
+type CreateUserResponse = CreateUserResponseTypes;
+type CreateUserRequest = CreateUserRequestTypes;
+type CreateUserReturn = Promise<CreateUserResponse>;
+type CreateUserProps = CreateUserRequest;
+
+const createUser = async ({
+  department,
+  firstName,
+  lastName,
+  password,
+  position,
+  role,
+  username,
+}: CreateUserProps): CreateUserReturn => {
+  const body: CreateUserRequest = {
+    department,
+    firstName,
+    lastName,
+    password,
+    position,
+    role,
+    username,
+  };
+
+  const { data } = await request<CreateUserResponse, CreateUserRequest>(apiUrls.users.index(), 'POST', {}, body);
+
+  return data;
+};
+
+type DeleteUserResponse = DeleteUserResponseType;
+type DeleteUserRequest = object;
+type DeleteUserReturn = Promise<DeleteUserResponse>;
+type DeleteUserProps = { userId: string };
+
+const deleteComment = async ({ userId }: DeleteUserProps): DeleteUserReturn => {
+  const { data } = await request<DeleteUserResponse, DeleteUserRequest>(apiUrls.users.index(userId), 'DELETE');
+
+  return data;
+};
+
+export const modify = {
+  post: createUser,
+  delete: deleteComment,
 };
