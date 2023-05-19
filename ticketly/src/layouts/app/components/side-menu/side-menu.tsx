06144841be746d6edf7a26db5d84d2004e85fb25
Kamil <k.wypych@pap.pl>
2023-05-19 23:31:54 +0200

    refactor: menu elements are displayed if user has proper role

--- ticketly/src/layouts/app/components/side-menu/side-menu.tsx
+++ ticketly/src/layouts/app/components/side-menu/side-menu.tsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { routesPrivileges } from 'privileges';
 import { NavLink } from 'react-router-dom';
+import { checkPrivileges } from 'utils';
 
 import { menuElements, MenuList, Wrapper } from '.';
 import { Logo, UserSection } from './components';
@@ -10,14 +12,18 @@ export const SideMenu = () => {
       <Logo />
 
       <MenuList>
-        {menuElements.map(({ id, title, path, icon }) => (
-          <li key={id}>
-            <NavLink to={path}>
-              <FontAwesomeIcon icon={icon} />
-              {title}
-            </NavLink>
-          </li>
-        ))}
+        {menuElements.map(({ id, title, path, icon }) => {
+          const privilegedRoles = routesPrivileges[path];
+
+          return checkPrivileges(privilegedRoles) ? (
+            <li key={id}>
+              <NavLink to={path}>
+                <FontAwesomeIcon icon={icon} />
+                {title}
+              </NavLink>
+            </li>
+          ) : null;
+        })}
       </MenuList>
 
       <UserSection />
