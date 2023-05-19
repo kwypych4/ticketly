559437bc7ed79a2f2fee05d0b4efd27fe00d7a6e
Kamil <k.wypych@pap.pl>
2023-05-19 13:47:46 +0200

    refactor: modified form styles

--- ticketly/src/styles/components/form.ts
+++ ticketly/src/styles/components/form.ts
@@ -17,9 +17,16 @@ export const form = css`
     }
   }
 
-  .ant-form-item {
-    margin-bottom: 6px;
+  .ant-form-item-control-input-content {
+    gap: 18px;
+    .ant-form-item:first-of-type {
+      flex-grow: 1;
+    }
+    .ant-form-item {
+      margin-bottom: 6px;
+    }
   }
+
   .ant-form-item-label {
     text-align: start !important;
   }
@@ -27,5 +34,14 @@ export const form = css`
   .ant-form-item-control {
     max-width: unset !important;
     min-height: unset !important;
+  }
+
+  .ant-form-item-control-input-content {
+    display: flex;
+
+    button {
+      margin-top: 32px;
+      height: 32px;
+    }
   }
 `;
