0fe46ef2f42528b1f34c70afaf57da0e0d856d2e
Kamil <k.wypych@pap.pl>
2023-05-19 14:30:27 +0200

    refactor: upgraded user global store

--- ticketly/src/store/user.ts
+++ ticketly/src/store/user.ts
@@ -3,9 +3,15 @@ import { create } from 'zustand';
 type UserStateType = {
   userId: string;
   username: string;
+  firstName: string;
+  lastName: string;
+  role: string;
 };
 
 export const useUserStore = create<UserStateType>(() => ({
   userId: '',
   username: '',
+  firstName: '',
+  lastName: '',
+  role: '',
 }));
