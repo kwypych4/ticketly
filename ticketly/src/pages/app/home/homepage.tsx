b6bf0c2dddbf65c4ec9c5700e12f98a0ebab2b31
Kamil <k.wypych@pap.pl>
2023-05-20 12:22:22 +0200

    feat: displaying tables in dashboard

--- ticketly/src/pages/app/home/homepage.tsx
+++ ticketly/src/pages/app/home/homepage.tsx
@@ -1,12 +1,43 @@
-import { PageContent, PageTitle, PageWrapper } from 'styles';
+import { api } from 'api';
+import { TableWrapper, TicketsBriefTable } from 'components';
+import { PrivateWrapper } from 'components/private-wrapper';
+import { useCustomQuery } from 'hooks';
+import { useState } from 'react';
+import { useUserStore } from 'store';
+import { PageContent, PageItem, PageTitle, PageWrapper } from 'styles';
+import { RequestParamsType } from 'types';
 
 export const Homepage = () => {
+  const { userId } = useUserStore.getState();
+  const [options, setOptions] = useState<RequestParamsType>({ page: 1, limit: 13 });
+  const ticketsListQuery = useCustomQuery(['ticketListOwner', options], () =>
+    api.tickets.list.fetch({ options, owner: userId })
+  );
+  const assignedTicketsListQuery = useCustomQuery(['ticketListAssigned', options], () =>
+    api.tickets.list.fetch({ options, engineer: userId })
+  );
+
   return (
     <PageWrapper>
       <PageTitle>
         <h2>Dashboard</h2>
       </PageTitle>
-      <PageContent />
+      <PageContent>
+        <PrivateWrapper privilegedRoles={['admin', 'engineer']}>
+          <PageItem>
+            <h3>Tickets assigned to you</h3>
+            <TableWrapper hasWrapper={false} setOptions={setOptions} data={assignedTicketsListQuery.data}>
+              <TicketsBriefTable assignedToEngineer />
+            </TableWrapper>
+          </PageItem>
+        </PrivateWrapper>
+        <PageItem>
+          <h3>Your tickets</h3>
+          <TableWrapper hasWrapper={false} setOptions={setOptions} data={ticketsListQuery.data}>
+            <TicketsBriefTable />
+          </TableWrapper>
+        </PageItem>
+      </PageContent>
     </PageWrapper>
   );
 };
