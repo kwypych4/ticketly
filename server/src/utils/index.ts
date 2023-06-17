3f11c59f6a0787cd93a7fccf04ba439928a5d895
Kamil <k.wypych@pap.pl>
2023-06-17 13:15:36 +0200

    refactor: added export

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './error-handler';
 export * from './hash-password';
+export * from './initial-user-creator';
 export * from './remove-all-user-sessions';
 export * from './slugify';
 export * from './tokens';
