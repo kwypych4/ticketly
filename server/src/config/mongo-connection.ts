eaa653f6920f3d6f0161f47bf7eb71570340c4bb
Kamil <k.wypych@pap.pl>
2023-06-17 13:14:24 +0200

    refactor: secured mongo connection and added intializing admin user

--- server/src/config/mongo-connection.ts
+++ server/src/config/mongo-connection.ts
@@ -1,13 +1,21 @@
 import { logger } from 'logger';
 import mongoose from 'mongoose';
+import { initialUserCreator } from 'utils';
 
 import { environment } from './environment';
 
 export const mongoConnect = () => {
-  mongoose.connect(environment.mongoPath as string);
+  mongoose.connect(environment.mongoPath as string, {
+    user: environment.mongoDbUser,
+    pass: environment.mongoDbPassword,
+    dbName: 'ticketly',
+  });
 
   const db: mongoose.Connection = mongoose.connection;
 
   db.on('error', (error) => logger.error(error));
-  db.once('connected', () => logger.info('Connected to DB'));
+  db.once('connected', () => {
+    logger.info('Connected to DB');
+    initialUserCreator();
+  });
 };
