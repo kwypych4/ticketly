b255bc2bf72d311b32cccd85327a8126387450fd
Kamil <k.wypych@pap.pl>
2023-08-22 19:12:01 +0200

    refactor: destructured component config

---
+++ ticketly/src/components/tables/tickets/tickets.data.tsx
@@ -0,0 +1,42 @@
+import { ColumnsType } from 'antd/es/table';
+import { NavLink } from 'react-router-dom';
+import { TicketsListTypes } from 'types';
+import { convertMinutes } from 'utils';
+
+export const columns: ColumnsType<TicketsListTypes> = [
+  {
+    title: 'Reporting user',
+    dataIndex: 'ownerName',
+    key: 'ownerName',
+  },
+  {
+    title: 'Problem',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text, record) => <NavLink to={record.id}>{text}</NavLink>,
+  },
+  {
+    title: 'Assigned To',
+    dataIndex: 'engineer',
+    key: 'engineer',
+    render: (text) => {
+      if (text) return text;
+
+      return 'Not assigned';
+    },
+  },
+  {
+    title: 'Time spent',
+    dataIndex: 'timeSpent',
+    key: 'timeSpent',
+    render: (text) => convertMinutes(text),
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (text) => {
+      return `${String(text)[0].toLocaleUpperCase()}${String(text).slice(1)}`;
+    },
+  },
+];
--- ticketly/src/components/tables/tickets/tickets.tsx
+++ ticketly/src/components/tables/tickets/tickets.tsx
@@ -1,46 +1,7 @@
 import { Table } from 'antd';
-import { ColumnsType } from 'antd/es/table';
-import { NavLink } from 'react-router-dom';
-import { TicketsListTypes, TicketsTableProps } from 'types';
-import { convertMinutes } from 'utils';
+import { TicketsTableProps } from 'types';
 
-const columns: ColumnsType<TicketsListTypes> = [
-  {
-    title: 'Reporting user',
-    dataIndex: 'ownerName',
-    key: 'ownerName',
-  },
-  {
-    title: 'Problem',
-    dataIndex: 'title',
-    key: 'title',
-    render: (text, record) => <NavLink to={record.id}>{text}</NavLink>,
-  },
-  {
-    title: 'Assigned To',
-    dataIndex: 'engineer',
-    key: 'engineer',
-    render: (text) => {
-      if (text) return text;
-
-      return 'Not assigned';
-    },
-  },
-  {
-    title: 'Time spent',
-    dataIndex: 'timeSpent',
-    key: 'timeSpent',
-    render: (text) => convertMinutes(text),
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-    key: 'status',
-    render: (text) => {
-      return `${String(text)[0].toLocaleUpperCase()}${String(text).slice(1)}`;
-    },
-  },
-];
+import { columns } from './tickets.data';
 
 export const TicketsTable = ({ setOptions, data, pagination, isLoading }: TicketsTableProps) => {
   return (
